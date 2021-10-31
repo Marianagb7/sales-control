a31de5798e4e4040fc65c218af924f3b27eb8447
Mariana Gómez Betancourt <marisgob777@hotmail.com>
2021-10-31 13:26:45 -0500

    Add H_014 Listar productos

---
+++ frontend-sales/src/components/products/ModalProduct.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap";
+
+const ModalProduct = () => {
+ 
+    return (
+        <div>
+            <Modal>
+                <ModalHeader style={{display: 'block'}}>
+                    <span style={{float: 'right'}}>x</span>
+                </ModalHeader>
+                <ModalBody>
+                    <div className="form-group">
+                        <label htmlFor="sku">Sku</label>
+                        <input className="form-control" type="text" name="sku"id="sku" readOnly
+                        onChange={this.handleChange} />
+                        <label htmlFor="nombre">Nombre</label>
+                        <input className="form-control" type="text" name="name" id="name" />
+                        <br/>
+                        <label htmlFor="descripcion">Descripción</label>
+                        <input className="form-control" 
+                        type="text" name="descripcion" id="description"/>
+                        <br/>
+                        <label htmlFor="precio">Precio</label>
+                        <input className="form-control" type="number"
+                        name="price" id="price"></input>
+                        <br/>
+                        <label htmlFor="estado">Estado</label>
+                        <select name="available">
+                            <option value="disponible">Disponible</option>                            
+                            <option value="nodisponible">No Disponible</option>
+                        </select>      
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-success">Actualizar</button>
+                    <button className="btn btn-danger">Cancelar</button>
+                </ModalFooter>
+            </Modal>
+            
+            
+
+        </div>        
+    )
+}
+
+export default ModalProduct;
+
--- frontend-sales/src/components/products/ProductList.jsx
+++ frontend-sales/src/components/products/ProductList.jsx
@@ -1,48 +1,71 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import ModalProduct from "./ModalProduct";
+
 import { Table } from "reactstrap";
 
-const ProductList = () => {  
 
-  
-    
-    return (
-        <div>
-            
-            <Table striped bordered hover size="sm">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Sku</th>
-      <th>Nombre</th>
-      <th>Descripción</th>
-      <th>Precio</th>
-      <th>Estado</th>
-      <th>Fecha de actualización</th>
-      <th>Actualizar</th>
-      <th>Eliminar</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    
-    
-  </tbody>
-</Table>
-             
-            
+const url = "http://localhost:4000/api/products"
 
-        </div>
-    );
+class ProductLIst extends Component  {
+state = {
+  data: []
 }
 
-export default ProductList;
+peticionGet = () => {
+axios.get(url).then(response => {
+  this.setState({data: response.data});
+})
+}
+  componentDidMount () {
+    this.peticionGet();
+
+  }
+  render () {
+    return (
+      <div>
+        <Table striped bordered hover size="sm">
+          <thead className="text-center">
+            <tr>             
+              <th className="col-sm-1">Sku</th>
+              <th className="col-sm-1">Nombre</th>
+              <th className="col-sm-1">Descripción</th>
+              <th className="col-sm-1">Precio</th>
+              <th className="col-sm-1">Estado</th>
+              <th className="col-sm-1">Acciones</th>                  
+            </tr>              
+          </thead>
+          <tbody>
+            {this.state.data.map(product => {
+              return (
+                <tr className="lead text-center  fs-5 h4 ">
+              <td className="fw-bold">{product.sku}</td>
+              <td className="fw-bolder">{product.name}</td>
+              <td className="fw-bolder">{product.description}</td>
+              <td className="fw-bolder">{new Intl.NumberFormat("en-En").format(product.price)}</td>
+              <td className="fw-bolder">{product.available}</td> 
+              <td> 
+                  <button className="btn btn-dark" onClick={()=>{this.seleccionarProduct(product); this.modalInsertar()}}>< FontAwesomeIcon icon={faEdit}/></button>
+                  {"   "} 
+                  <button className="btn btn-warning" onClick={()=>{this.seleccionarProduct(product); this.setState({modalEliminar: true})}}><FontAwesomeIcon icon={faTrashAlt}/></button>                    
+               </td>     
+               
+                </tr>
+              )
+            })}            
+
+          </tbody>
+        </Table>
+        <ModalProduct></ModalProduct>
+        
+      </div>
+
+
+    );
+
+  }
+}
+export default ProductLIst;
