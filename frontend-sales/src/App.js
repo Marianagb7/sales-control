5ae018638c2d201e589cc2a17aa7e95bc4e378da
Paula Saavedra <paula.saavedraca@amigo.edu.co>
2021-11-07 16:32:13 -0500

    app modificado

--- frontend-sales/src/App.js
+++ frontend-sales/src/App.js
@@ -4,48 +4,56 @@ import PublictLayout from './layout/PublictLayout';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Index from './pages/Index'
+import Usuarios from './pages/admin/Usuarios';
 import Productos from './pages/admin/Productos';
-import { Auth0Provider } from '@auth0/auth0-react';
+import GesProductos from './pages/admin/GesProductos'
+import Ventas from './pages/admin/Ventas';
+import GesVentas from './pages/admin/GesVentas'
+import Sidebar from './layout/Sidebar'
+import NavbarNav from './layout/NavbarNav'
+
 import './styles/App.scss';
+
 
 function App() {
   return (    
-    <Auth0Provider
-      domain="misiontic-appventas.us.auth0.com"
-      clientId="hD6ENm2krarGEQqipTyNHig7qEfm3m4x"
-      redirectUri={window.location.origin}
-      audience= 'api-autenticacion-appventas-mintic'
-    > 
-      <Router>
-        <Switch>
-          <Route path={['/admin', '/admin/productos']}>
-            <Switch>
-              <Route path='/admin/productos'>
-                <Productos/>
-              </Route>
-              <Route path='/admin'>
-                <Admin/>
-              </Route>
-            </Switch>
-          </Route>
-          <Route path={['/login']}> 
+    <Router>
+      <Switch>
+        <Route path={['/admin', '/admin/usuarios', '/admin/productos',
+         '/admin/gestionproductos', '/admin/ventas', '/admin/gestionventas']}>
           <Switch>
-            <Route path='/login'>
-              <Login/> 
-            </Route>  
-          </Switch> 
-          </Route>                       
-          <Route path={['/']}>
-            <PublictLayout>
-              <Route path='/'>
-              <Index/>
-              </Route>
-            </PublictLayout>
-          </Route>        
-        
-        </Switch>
-      </Router>
-    </Auth0Provider>     
+             <div className="flex">
+                <Sidebar/>
+                  <div className= "content w-100">
+                      <NavbarNav/>            
+                       <Route path="/admin" exact={true} component={Admin} />
+                       <Route path="/admin/usuarios" exact={true} component={Usuarios} />
+                       <Route path="/admin/productos" exact={true} component={Productos} />
+                      <Route path="/admin/gestionproductos" exact={true} component={GesProductos} />
+                      <Route path="/admin/ventas" exact={true} component={Ventas} />
+                      <Route path="/admin/gestionventas" exact={true} component={GesVentas} />
+                   </div>          
+
+              </div>
+          </Switch>        
+        </Route>        
+        <Route path={['/login']}> 
+        <Switch>
+          <Route path='/login'>
+            <Login/> 
+          </Route>  
+        </Switch> 
+        </Route>                       
+        <Route path={['/']}>
+          <PublictLayout>
+            <Route path='/'>
+            <Index/>
+            </Route>
+         </PublictLayout>
+        </Route>      
+               
+      </Switch>
+    </Router>     
         
         
   );
