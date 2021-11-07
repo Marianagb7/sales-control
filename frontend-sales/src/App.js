c27c82b92fe94f29f313204c28e79be1ac5a478f
Paula Saavedra <paula.saavedraca@amigo.edu.co>
2021-11-07 18:18:12 -0500

    cambio en las rutas del app

--- frontend-sales/src/App.js
+++ frontend-sales/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PublictLayout from './layout/PublictLayout';
+import PublictLayout from './layout/publictlayaut/PublictLayout';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Index from './pages/Index'
@@ -8,53 +8,61 @@ import Usuarios from './pages/admin/Usuarios';
 import Productos from './pages/admin/Productos';
 import GesProductos from './pages/admin/GesProductos'
 import Ventas from './pages/admin/Ventas';
-import GesVentas from './pages/admin/GesVentas'
+import GesVentas from './pages/admin/GesVentas';
+import NavbarNav from './layout/NavbarNav';
 import Sidebar from './layout/Sidebar'
-import NavbarNav from './layout/NavbarNav'
-
+import { Auth0Provider } from '@auth0/auth0-react';
 import './styles/App.scss';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
-  return (    
-    <Router>
-      <Switch>
-        <Route path={['/admin', '/admin/usuarios', '/admin/productos',
-         '/admin/gestionproductos', '/admin/ventas', '/admin/gestionventas']}>
-          <Switch>
-             <div className="flex">
-                <Sidebar/>
-                  <div className= "content w-100">
-                      <NavbarNav/>            
-                       <Route path="/admin" exact={true} component={Admin} />
-                       <Route path="/admin/usuarios" exact={true} component={Usuarios} />
-                       <Route path="/admin/productos" exact={true} component={Productos} />
-                      <Route path="/admin/gestionproductos" exact={true} component={GesProductos} />
-                      <Route path="/admin/ventas" exact={true} component={Ventas} />
-                      <Route path="/admin/gestionventas" exact={true} component={GesVentas} />
-                   </div>          
-
-              </div>
-          </Switch>        
-        </Route>        
-        <Route path={['/login']}> 
+  return ( 
+    <Auth0Provider
+      domain="misiontic-appventas.us.auth0.com"
+      clientId="hD6ENm2krarGEQqipTyNHig7qEfm3m4x"
+      redirectUri={window.location.origin}
+      audience= 'api-autenticacion-appventas-mintic'
+    >  
+      <Router>
         <Switch>
-          <Route path='/login'>
-            <Login/> 
-          </Route>  
-        </Switch> 
-        </Route>                       
-        <Route path={['/']}>
-          <PublictLayout>
-            <Route path='/'>
-            <Index/>
-            </Route>
-         </PublictLayout>
-        </Route>      
-               
-      </Switch>
-    </Router>     
-        
+          <Route path={['/admin', '/admin/usuarios', '/admin/productos',
+          '/admin/gestionproductos', '/admin/ventas', '/admin/gestionventas']}>
+            <PrivateRoute>
+              <Switch>
+                <div className="flex">
+                  <Sidebar/>
+                  <div className= "content w-100">
+                    <NavbarNav/>         
+                    <Route path="/admin" exact={true} component={Admin} />
+                    <Route path="/admin/usuarios" exact={true} component={Usuarios} />
+                    <Route path="/admin/productos" exact={true} component={Productos} />
+                    <Route path="/admin/gestionproductos" exact={true} component={GesProductos} />
+                    <Route path="/admin/ventas" exact={true} component={Ventas} />
+                    <Route path="/admin/gestionventas" exact={true} component={GesVentas} />
+                  </div>
+                </div>
+              </Switch> 
+            </PrivateRoute>   
+          </Route>        
+          <Route path={['/login']}> 
+          <Switch>
+            <Route path='/login'>
+              <Login/> 
+            </Route>  
+          </Switch> 
+          </Route>                       
+          <Route path={['/']}>
+            <PublictLayout>
+              <Route path='/'>
+              <Index/>
+              </Route>
+          </PublictLayout>
+          </Route>      
+                
+        </Switch>
+      </Router>     
+    </Auth0Provider>     
         
   );
 }
