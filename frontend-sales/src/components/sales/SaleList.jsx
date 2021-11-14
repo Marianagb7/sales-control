5d6abe130a5c51ae1b2c2cd059ff5452f8cb31a2
PaulaSaavedra95 <89286123+PaulaSaavedra95@users.noreply.github.com>
2021-11-14 04:16:37 -0500

    Update SaleList.jsx

--- frontend-sales/src/components/sales/SaleList.jsx
+++ frontend-sales/src/components/sales/SaleList.jsx
@@ -12,7 +12,7 @@ import { Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap";
 
 function SaleList(){
 
-    const url ="https://ebrovinos-misiontic.herokuapp.com/api/sales";
+    const url ="https://localhost:3000";
 
     const [sales, setSales]=useState([]);
     const [tablesales, setTablesales]=useState([]);
